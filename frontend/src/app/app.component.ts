35fcec2c310a00275eb5c4634f247a269bb82165
caelebk <khocaeleb@gmail.com>
2025-12-02 15:44:20 -0800

    vibe init

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'frontend';
+  darkMode = false;
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+  }
 }
