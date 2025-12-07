391c27efae075614a8698937f597abcf7abe1043
Caeleb <khocaeleb@gmail.com>
2025-12-07 02:04:40 -0800

    updated item-card to use signals

--- frontend/src/app/pages/inventory/inventory-components/item-card/item-card.component.ts
+++ frontend/src/app/pages/inventory/inventory-components/item-card/item-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output } from '@angular/core';
+import { Component, Output, input, computed, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@jsverse/transloco';
 import { DialogModule } from 'primeng/dialog';
@@ -17,32 +17,27 @@ import { EditItemFormComponent } from '../edit-item-form/edit-item-form.componen
   styles: [':host { display: block; height: 100%; }']
 })
 export class ItemCardComponent {
-  @Input() item!: Item;
+  item = input.required<Item>();
   @Output() delete = new Subject<void>();
   @Output() update = new Subject<Item>();
 
-  public expired: boolean = false;
-  public itemProgress: number = 0;
-  public timeRemaining: string = '';
-  public displayNoteDialog: boolean = false;
-  public displayEditDialog: boolean = false;
+  public expired = computed(() => isExpired(this.item()));
+  public itemProgress = computed(() => itemProgress(this.item()));
+  public timeRemaining = computed(() => getTimeDifferenceString(new Date(), this.item().bestBeforeDate));
+
+  public displayNoteDialog = signal(false);
+  public displayEditDialog = signal(false);
 
   showNote() {
-    this.displayNoteDialog = true;
+    this.displayNoteDialog.set(true);
   }
   
   showEdit() {
-    this.displayEditDialog = true;
+    this.displayEditDialog.set(true);
   }
 
   onEditItem(updatedItem: Item) {
     this.update.next(updatedItem);
-    this.displayEditDialog = false;
-  }
-
-  ngOnInit(): void {
-    this.expired = isExpired(this.item);
-    this.itemProgress = itemProgress(this.item);
-    this.timeRemaining = getTimeDifferenceString(new Date(), this.item.bestBeforeDate); 
+    this.displayEditDialog.set(false);
   }
 }
